6ed630f8549ee356dd73adbca835b16bd34470f0
Maksim Ulyanov <umaxim2000@mail.ru>
2022-07-06 14:59:31 +0300

    PRJ51-11:Implemented terrafrom scripts to cover AWS application deployment

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   required_version = "~> 1.0"
   backend "s3" {
-    bucket = "donorun-tf-state-storage"
+    bucket = "donorun-tf-state"
     key    = "donorun.tfstate"
     region = "us-east-2"
   }
